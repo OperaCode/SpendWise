b3d8aa3a334cb5dcf9f5e26b026b58c85907b494
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-07-19 23:12:55 +0100

    livelink

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseURI = "http://localhost:3000"; 
+const baseURI = "https://trackit-tfj4.onrender.com"; 
+
 
 export const apiSlice = createApi({
   reducerPath: 'api', 
